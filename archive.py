dc6f09ce47a102072c52380b48ec40b52abfb06d
serj <serj@debian-lsa>
2016-12-28 00:01:15 +0300

    add arch counter

--- archive.py
+++ archive.py
@@ -8,20 +8,23 @@ class Archive:
     setting = False
     parent_catalog = False
     files_counter = 0
+    catalog = ''
+    separator = "|"
 
     def __init__(self, setting):
         if not setting:
             print("Нет настроек для архивации")
         self.setting = setting
+        self.__get_catalog()
 
     def __get_catalog(self):
-        return (self.setting['path']).split('/')[-1]
+        self.catalog = (self.setting['path']).split('/')[-1]
 
     def __get_parent_catalog(self):
-        return (self.setting['path']).split(self.__get_catalog())[0]
+        return (self.setting['path']).split(self.catalog)[0]
 
     def __init_archive(self):
-        name = self.setting['zip_path'] + 'backup|' + str(datetime.datetime.now()) + ".zip"
+        name = self.setting['zip_path'] + self.catalog + self.separator + str(datetime.datetime.now()) + ".zip"
         return zipfile.ZipFile(name, 'w')
 
     def __get_catalog_content(self):
@@ -41,7 +44,6 @@ class Archive:
 
         os_sym = self.__get_os_type()
 
-
         for root, dirs, files in content:
             for file in files:
                 current_dir = root.split(parent_catalog)[-1] + os_sym
@@ -50,3 +52,23 @@ class Archive:
                 self.files_counter = self.files_counter + 1
 
         arch.close()
+
+    def inspection_archive(self):
+        arch_counter = 0
+        data_files = os.listdir(self.setting['zip_path'])
+        for value in data_files:
+            if value.find(self.catalog + self.separator) != (-1):
+                arch_counter = arch_counter + 1
+                # print(arch_counter)
+        return arch_counter
+        # print(str(data_files))
+    def delete_arch(self):
+
+        data = {}
+        data_files = os.listdir(self.setting['zip_path'])
+        for value in data_files:
+            if value.find(self.catalog + self.separator) != (-1):
+                data[str((value.split(self.separator)[1]).split('.zip')[0])] = value
+
+        print(str(data))
+
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 from archive import Archive
 
 print("Подготовка к архивации")
-print("Проверка прав доступа. Только root может проводить архивацию")
+# print("Проверка прав доступа. Только root может проводить архивацию")
 # if os.getlogin() == 'root':
 #     print("Всё ОК")
 # else:
@@ -21,38 +21,9 @@ setting = json.loads(open('setting.json').read())
 for key in setting:
     # print(key)
     archive = Archive(setting[key])
-    archive.archiving()
-    print(setting[key])
-
-
-
-# catalog_list = os.listdir(setting['path'])
-# print("Выбранный каталог")
-# catalog = (setting['path']).split('/')[-1] *
-# catalog_parent = (setting['path']).split(catalog)[0] *
-# os.chdir(catalog_parent)
-# # print(catalog)
-# # print(catalog_parent)
-# # print(catalog_list)
-#
-# name = setting['zip_path'] + 'test|' + str(current_date) + ".zip"
-#
-# print("Подготовка архива")
-#
-# arch = zipfile.ZipFile(name, 'w')
-#
-# print("Запись в архив")
-# # print(name)
-#
-# user_dir = os.walk(setting['path'])
-# files_counter = 0
-# for root, dirs, files in user_dir:
-#     for file in files:
-#         my_dir = root.split(catalog_parent)[-1] + "/"
-#         print("Архивация " + my_dir + " " + str(files))
-#         arch.write(os.path.join(my_dir, file))
-#         files_counter = files_counter + 1
-# arch.close()
-#
-# print("Заархивированно " + str(files_counter))
-# print("---------Готово---------")
+    # archive.archiving()
+    # print(setting[key])
+    # print("Заархивированно " + str(archive.files_counter))
+    archive.inspection_archive()
+    archive.delete_arch()
+print("---------Готово---------")
