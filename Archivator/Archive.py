8a3783b5e5ab60173a6723946c69722539c212fe
Jluct <panaev02@gmail.com>
2017-01-23 17:33:05 +0300

    Relis archiving files

--- Archivator/Archive.py
+++ Archivator/Archive.py
@@ -10,21 +10,24 @@ class Archive:
         self.setting = setting
         # self.__get_catalog()
         self.__archive = ArchiveFile()
+        # Инициализация архива ?
+        self.__archive.init_archive(self.setting['arch_path'], self.setting['archive_name'])
 
     def archiving(self):
-        # Инициализация архива ?
-        self.__archive.init_archive(self.setting['archive_name'])
-
-        # Запись в архив по файлам
-        self.__archive.archiving(self.setting['path'])
+        # Запись в архив
+        self.__archive.archiving(self.setting['files_path'])
         # Закрытие архива
         # self.__archive(self.setting['path'], self.setting['zip_path'])
-
-    def inspection_archive(self):
-        self.__archive.inspection_archive()  # !
-
-    def delete_unnecessary_arch(self):
-        self.__archive.delete_unnecessary_arch(self.setting['quantity'])
+        pass
 
     def archiving_additional_files(self):
+        if 'additional_files' in self.setting:
+            self.__archive.archiving_addition_files(self.setting['additional_files'])
+
+    def inspection_archive(self):
+        # self.__archive.inspection_archive()  # !
+        pass
+
+    def delete_unnecessary_arch(self):
+        # self.__archive.delete_unnecessary_arch(self.setting['quantity'])
         pass
